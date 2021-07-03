3272d7894ce512a25ac5582126a78eb8c525c1b8
Biel <bielp07@gmail.com>
2021-07-03 10:31:38 -0300

    Add `app.py`, `test.py` and `templates` directory.
    Create unit tests and change "Git" to Github on readme.

---
+++ test.py
@@ -0,0 +1,18 @@
+from app import app
+import unittest
+
+class Test(unittest.Testcase):
+    def setup(self):
+        self.app app.test_client()
+
+    def testReq(self):
+        result = self.app.get("/")
+        self.assertEqual(result.status_code, 200)
+
+    def testContent(self):
+        result = self.app.get("/")
+        self.assertRegex(result.data.decode(), "Devops Laboratory")
+
+if __name__ = "__main__":
+    print("Initializing tests.")
+    unittest.main(verbosity = 2)
